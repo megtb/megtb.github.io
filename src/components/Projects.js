d247508351e916232ef8897727dd9831f7a6abd0
megtb <megbickerstaff@gmail.com>
2023-05-19 12:52:09 -0400

    Styles Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,41 +4,93 @@ import { projects } from "../data";
 export default function Projects() {
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
-      <div className="container px-5 py-10 mx-auto text-left lg:px-40">
-        <div className="flex flex-col w-full mb-20">
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Projects
-          </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            placeholder
-          </p>
-        </div>
-        <div className="flex flex-wrap -m-4">
-          {projects.map((project) => (
-            <a
-              href={project.link}
-              key={project.image}
-              className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
-                <img
+    <div className="container px-5 py-10 mx-auto text-left lg:px-40">
+    <div>
+      <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+             Projects
+      </h1>
+    </div>
+         <div className="flex flex-col w-full mb-20">
+            {projects.map((project) => (
+              <section>
+                <div className="flex relative">
+                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-contain object-center"
+                  className="relative w-1/2 inset-0 object-contain m-8"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                    {project.subtitle}
-                  </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
-                    {project.title}
-                  </h1>
-                  <p className="leading-relaxed">{project.description}</p>
-                </div>
-              </div>
-            </a>
-          ))}
-        </div>
+      <div class="p-6">
+        <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">
+          {project.subtitle}
+      </h2>
+      <div class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50 hover:bg-indigo-500">
+        <a href={project.link}>{project.title}</a>
       </div>
-    </section>
+        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          {project.description}
+        </p>
+        <a
+          type="button"
+          class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full"
+          href={project.github}>
+            Project Github
+        </a>
+        {project.youtube && 
+          <a
+          type="button"
+          class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-full"
+          href={project.youtube}>
+            Video Walkthrough
+        </a>
+        }
+      </div>
+    </div>
+  </section>
+  ))}
+  </div>
+  </div>
+  
+</section>
+    
+    // <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+    //   <div className="container px-5 py-10 mx-auto text-left lg:px-40">
+    //     <div className="flex flex-col w-full mb-20">
+    //       <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+    //         Projects
+    //       </h1>
+    //       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+    //         placeholder
+    //       </p>
+    //     </div>
+    //     <div className="flex flex-wrap -m-4">
+    //       {projects.map((project) => (
+    //         <section>
+    //           <div>
+    //           href={project.link}
+    //           key={project.image}
+    //           className="sm:w-1/2 w-100 p-4"
+    //           </div>
+              
+    //           <div className="flex relative">
+    //             <img
+    //               alt="gallery"
+    //               className="absolute inset-0 w-full h-full object-contain"
+    //               src={project.image}
+    //             />
+    //             <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+    //               <h2 className="tracking-widest text-sm title-font font-medium text-indigo-400 mb-1">
+    //                 {project.subtitle}
+    //               </h2>
+    //               <h1 className="title-font text-lg font-medium text-white mb-3">
+    //                 {project.title}
+    //               </h1>
+    //               <p className="leading-relaxed">{project.description}</p>
+    //             </div>
+    //           </div>
+    //           </section>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </section>
   );
 }
--- src/data.js
+++ src/data.js
@@ -6,6 +6,8 @@ export const projects = [
       "Backend Protokol Puzzles is a full-stack e-commerce app for a puzzle company",
     image: "./BackendProtokol.png",
     link: "https://backend-protokol-puzzles.onrender.com/",
+    github: "https://github.com/graceshpopper-team-backend-protokol",
+    youtube: null
   },
   {
     title: "Yummify",
@@ -13,27 +15,13 @@ export const projects = [
     description:
       "Yummify is a full-stack e-commerce app ",
     image: "./Yummify.png",
-    link: "https://www.youtube.com/watch?v=K3fjTKBAuxE&t=31s",
+    link: "https://yummify-352b4.web.app/",
+    github: "https://github.com/yummify/yummify",
+    youtube: "https://www.youtube.com/watch?v=K3fjTKBAuxE&t=31s"
   },
   
 ];
 
-// export const testimonials = [
-//   {
-//     quote:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-//     image: "https://randomuser.me/api/portraits/men/1.jpg",
-//     name: "Jesse Hicks",
-//     company: "Zoozle",
-//   },
-//   {
-//     quote:
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-//     image: "https://randomuser.me/api/portraits/men/94.jpg",
-//     name: "Ruben Alvarez",
-//     company: "Dooble.io",
-//   },
-// ];
 
 export const skills = [
   "JavaScript",
